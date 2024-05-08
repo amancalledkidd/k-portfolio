c84b829f18dd2b3232c2bbd67eb8453116050e57
Kumani K <kumanikidd@gmail.com>
2024-05-08 06:38:08 +0100

    Landing setup

---
+++ src/components/LandingSection/LandingSection.tsx
@@ -0,0 +1,10 @@
+import './LandingSection.scss'
+
+const LandingSection = () => {
+    return (
+        <div className="landing-section">
+        </div>
+    );
+};
+
+export default LandingSection;
