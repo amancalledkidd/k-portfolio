d829828d3bd9dc57465905005f2bf25a59608edc
Kumani K <kumanikidd@gmail.com>
2024-05-08 06:38:24 +0100

    Dev type added

---
+++ src/types/Developer.ts
@@ -0,0 +1,15 @@
+type Developer = {
+    name: string;
+    description: string;
+    image: string;
+    role: string;
+    location: string;
+    email: string;
+    linkedin: string;
+    github: string;
+    
+    };
+
+
+
+    export default Developer;
