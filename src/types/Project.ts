715cb9efa10bd9431774df108e832763a1b666ff
Kumani K <kumanikidd@gmail.com>
2024-05-08 06:38:36 +0100

    project type added

---
+++ src/types/Project.ts
@@ -0,0 +1,11 @@
+type Project = {
+    id: number;
+    name: string;
+    description: string;
+    image: string;
+    github_url: string;
+    live_url: string;
+    tech_stack: string[];
+    };
+
+export default Project;
