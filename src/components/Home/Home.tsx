c227e02a97bb4def478f05e5db962c3afcde221c
Kumani K <kumanikidd@gmail.com>
2024-05-08 06:37:50 +0100

    Home component set up

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.scss';
+import LandingSection from '../LandingSection/LandingSection';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <LandingSection />
+        </div>
+    );
+};
+
+export default Home;
