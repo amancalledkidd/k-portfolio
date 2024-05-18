ec5c264ca4696ef9a539d10b278bc6b2ea2bc8f3
Kumani K <kumanikidd@gmail.com>
2024-05-18 11:47:19 +0100

    improved skills section and text alignment

--- src/components/LandingSection/LandingSection.tsx
+++ src/components/LandingSection/LandingSection.tsx
@@ -1,8 +1,16 @@
 import './LandingSection.scss'
+import SkillList from '../SkillsList/SkillsList';
 
 const LandingSection = () => {
     return (
         <div className="landing-section">
+            <div className='landing-section__info'>
+                <h1 className='landing-section__info__dev-name'>KUMANI KIDD</h1>
+                <p className='landing-section__info__dev-role'>JUNIOR SOFTWARE ENGINEER</p>
+            </div>
+            <div className='landing-section__tech-stack'>
+                <SkillList />
+            </div>
         </div>
     );
 };
