6d57d8d2be649b2af6cff563db3479bc57955a19
Kumani K <kumanikidd@gmail.com>
2024-05-28 11:37:59 +0100

    desktop landing complete

--- src/components/LandingSection/LandingSection.tsx
+++ src/components/LandingSection/LandingSection.tsx
@@ -1,9 +1,14 @@
+/// <reference types="vite-plugin-svgr/client" />
 import './LandingSection.scss'
 import SkillList from '../SkillsList/SkillsList';
+import HexLattice from '../../assets/images/HEXAGONAL LATTICE Landing.svg?react';
 
 const LandingSection = () => {
     return (
         <div className="landing-section">
+            <div className='landing-section__img'>
+                <HexLattice />
+            </div>
             <div className='landing-section__info'>
                 <h1 className='landing-section__info__dev-name'>KUMANI KIDD</h1>
                 <p className='landing-section__info__dev-role'>JUNIOR SOFTWARE ENGINEER</p>
