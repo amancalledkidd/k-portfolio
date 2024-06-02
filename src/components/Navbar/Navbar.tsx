166189fef3d89861bb32f23bc7a39e2091b86cba
Kumani K <kumanikidd@gmail.com>
2024-06-02 05:17:51 +0100

    improved responsiveness

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,23 @@
+import React, { useState } from 'react';
 import './Navbar.scss';
+import dropdownIcon from '../../assets/icons/myLogo.png';
 
 const Navbar = () => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
     return (
         <div className='navbar'>
-            <div className='navbar__links'>
-                <a href='#my-journey'>My Journey</a>
-                <a href='#projects'>Projects</a>
-                <a href='#contact'>Contact</a>
+            <div className={`navbar__links ${isDropdownOpen ? 'navbar__links--open' : ''}`}>
+                <a href='#my-journey' onClick={toggleDropdown}>My Journey</a>
+                <a href='#projects' onClick={toggleDropdown}>Projects</a>
+                <a href='#contact' onClick={toggleDropdown}>Contact</a>
+            </div>
+            <div className='navbar__dropdown-icon' onClick={toggleDropdown}>
+                <img src={dropdownIcon} alt='dropdown-icon' />
             </div>
         </div>
     );
