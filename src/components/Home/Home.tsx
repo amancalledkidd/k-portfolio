9b6738417aa8fb0d99c0aa0be7cf34d6ca335eb4
Kumani K <kumanikidd@gmail.com>
2024-06-18 17:24:17 +0100

    home update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,19 @@
 import './Home.scss';
 import LandingSection from '../LandingSection/LandingSection';
+import AboutMe from '../AboutMe/AboutMe';
+import ProjectSection from '../ProjectSection/ProjectSection';
+import Navbar from '../Navbar/Navbar';
+import ContactSection from '../ContactSection/ContactSection';
+
 
 const Home = () => {
     return (
         <div className="home">
+            <Navbar />
             <LandingSection />
+            <AboutMe />
+            <ProjectSection />
+            <ContactSection />
         </div>
     );
 };
