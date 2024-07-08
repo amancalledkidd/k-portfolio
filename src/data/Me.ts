e154461eaa9ceeb5972bd79b81a4f2b8150117d9
Kumani K <kumanikidd@gmail.com>
2024-07-08 10:24:01 +0100

    deploy updates

--- src/data/Me.ts
+++ src/data/Me.ts
@@ -4,7 +4,7 @@ import profilePic from '../assets/images/Hex profile pic.png'
 
 const Me: Developer = {
     name: "Kumani Kidd",
-    description: "A passionate Junior Software Engineer with a deep-rooted love for technology. My journey began in the world of video technology, where I developed strong problem-solving skills and honed my teamwork abilities. Driven by an ever-growing fascination with software engineering, () I took a bold step, transitioning from manufacturing to a software development bootcamp. Now, I'm excited to delve deeper into this field with _nology, expanding my expertise in TypeScript, React, and other cutting-edge technologies. My commitment to continuous learning and my diverse skill set make me ready to take on new challenges.",
+    description: "I am a passionate Junior Software Engineer with a deep-rooted love for technology. My journey began in the world of video technology, where I built video processors. Here, I developed strong problem-solving skills and honed my teamwork abilities. Driven by a growing interest in software engineering, I decided to transition from manufacturing to software development. () I joined the Makers Academy Software Development Bootcamp, then followed that up with the _nology Junior Tech Engineer Accelerator. These 8 months of study gave me a strong foundation in full-stack software development. Now, I'm excited to delve deeper into this field and build upon these foundations. My commitment to continuous learning and my diverse skill set make me ready to take on new challenges.",
     image: profilePic,
     role: "Junior Software Engineer"
 };
